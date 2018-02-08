a7a0b2a807f8d9a6ab6481e05fb598fa7bb57a9e
StevanEarl <stevan.earl@asu.edu>
2018-02-08 14:10:17 -0700

    added updated content and heights data exercise for R intro

--- intro_to_R.R
+++ intro_to_R.R
@@ -1,27 +1,86 @@
-# Rstudio Environment ----
+
+# Rstudio environment -----------------------------------------------------
 
 Rstudio != R
 
 
-# R as a calculator ----
+# R as a calculator -------------------------------------------------------
 
 3 + 4
 7 * 5
 6 ^ 3
 
 
-# assignment ----
+# data structures ---------------------------------------------------------
+
+1. vector: sequence of data elements of the same type
+
+c(1,2,3)
+c(1,"two",3)
+
+c(1,2,3)[2]
+c(1,"two",3)[2]
+c(1,"two",3)[c(1,2)]
+
+a <- c(1,2,3)[2]
+a <- c(1,"two",3)[2]
+
+2. matrix: two-dimensional, rectangular arrangement of objects of the same type
+
+3. list: one-dimensional group of R objects
+
+n <- c(2, 3, 5) 
+n <- c(2, "three", 5) 
+s <- c("aa", "bb", "cc", "dd", "ee") 
+b <- c(TRUE, FALSE, TRUE, FALSE, FALSE) 
+
+x <- list(n, s, b, 3) # x contains copies of n, s, b
+
+4. factor: store predefined and categorical data
+
+x <- factor(c("single", "married", "married", "single"))
+
+x <- factor(c("single", "married", "married", "single"), 
+            levels = c("single", "married", "divorced"))
+
+6. data frame: two dimensional group of R vectors
+
+n <- c(2, 3, 5) 
+n <- c(2, "three", 5) 
+s <- c("aa", "bb", "cc") 
+b <- c(TRUE, FALSE, TRUE) 
+
+data.frame(n, s, b)
+data.frame(n = c(2, 3, 5), s = c("aa", "bb", "cc"), b = c(TRUE, FALSE, TRUE))
+data.frame(n = c(2, 3, 5), s = c("aa", "bb", "cc"), b = c(TRUE, FALSE, TRUE), z = 4)
+data.frame(n = c(2, 3, 5), s = c("aa", "bb", "cc"), b = c(TRUE, FALSE, TRUE), z = c(4, 8))
+
+ourFrame <- data.frame(n = c(2, 3, 5),
+                       s= c("aa", "bb", "cc"),
+                       b = c(TRUE, FALSE, TRUE),
+                       z = 4)
+
+class(ourFrame)
+str(ourFrame)
+names(ourFrame)
+
+ourFrame$n
+ourFrame$s
+ourFrame$b
+ourFrame$n[2]
+
+
+# assignment --------------------------------------------------------------
 
 All R statements where you create objects -- "assignments" -- have this form:
 objectName <- value
 and in my head I hear, e.g., "x gets 12".
 
+x <- 12 
 x <- 3 * 4
 
-note the object x in your workspace
 
-
-# functions ----
+# functions ---------------------------------------------------------------
 
 functionName(arg1 = val1, arg2 = val2, and so on)
 
@@ -39,7 +98,7 @@ mean(seq(1:10))
 round(mean(seq(1:10)), 0)
 
 
-# build your own function ----
+# build your own ----------------------------------------------------------
 
 convert farenheit to Kelvin:
   
@@ -53,14 +112,9 @@ fahr_to_kelvin(32)
 waterFreezesKelvin <- fahr_to_kelvin(32)
 
 
-# workspace ----
+# workspace ---------------------------------------------------------------
 
 careful to treat it as temporary storage
 
 
-# the canvas ----
-
-.R
-.Rmd
-
-# packages -----
+# packages ----------------------------------------------------------------
---
+++ working_with_data.R
@@ -0,0 +1,88 @@
+
+# import data -------------------------------------------------------------
+
+heightData <- read.csv('~/localRepos/meet_R/heights.csv',
+                       stringsAsFactors = F)
+
+heightData <- read.csv('~/localRepos/meet_R/heights.csv')
+
+
+# inspect data ------------------------------------------------------------
+
+class(heightData)
+
+str(heightData)
+
+# histogram
+hist(heightData$height)
+
+# nicer histogram
+hist(heightData$height,
+     main = 'histogram of heights',
+     xlab = 'inches')
+
+# just males
+hist(heightData[heightData$sex == 'Male',]$height,
+     main = 'histogram of male heights',
+     xlab = 'inches')
+
+# just females
+hist(heightData[heightData$sex == 'Female',]$height,
+     main = 'histogram of female heights',
+     xlab = 'inches')
+
+# side-by-side
+par(mfrow = c(1,2))
+hist(heightData[heightData$sex == 'Male',]$height,
+     main = 'male',
+     xlab = 'inches')
+
+hist(heightData[heightData$sex == 'Female',]$height,
+     main = 'female',
+     xlab = 'inches')
+dev.off()
+
+# add a normal curve
+heightHist <- hist(heightData$height,
+                   main = 'histogram of heights',
+                   xlab = 'inches')
+xfit <- seq(min(heightData$height),
+            max(heightData$height),
+            length = 50)
+yfit <- dnorm(xfit,
+              mean = mean(heightData$height),
+              sd = sd(heightData$height))
+yfit <- yfit * diff(heightHist$mids[1:2]) * length(heightData$height)
+lines(xfit, yfit, col = "black", lwd = 2)
+
+# test for normality
+shapiro.test(heightData$height)
+
+# box plot
+boxplot(height~sex,
+        data = heightData)
+
+
+
+# significance testing ----------------------------------------------------
+
+# are the height of men and women different?
+t.test(heightData[heightData$sex == 'Male',]$height, heightData[heightData$sex == 'Female',]$height)
+
+# with ANOVA
+heightsAnova <- aov(height ~ sex, data = heightData)
+summary(heightsAnova)
+
+str(heightsAnova)
+
+heightsAnova$coefficients # examine the coefficients
+heightsAnova$coefficients["sexMale"] # extract slope
+alpha <- heightsAnova$coefficients["sexMale"] # our model coeff
+
+
+# packages ----------------------------------------------------------------
+
+install.packages('readxl')
+library(readxl)
+
+fromExcel <- read_excel('~/localRepos/meet_R/heights_excel.xlsx')
